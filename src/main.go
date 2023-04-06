d9eac78a152eedcf43e47b1224db2ec2c3bd8299
rym49 <hatanakaryoma.130@gmail.com>
2023-04-07 02:30:50 +0900

    エラー処理の追加

--- src/main.go
+++ src/main.go
@@ -24,22 +24,30 @@ func main() {
 	var feed []Feed
 	for _, Site := range constant.SITE_LIST {
 		logger.Info("Search new feed", zap.String("サイト名", Site.Name))
-		f := SearchFeed(Site, time.Duration(-1*conf.Range)*time.Hour)
+		f, err := SearchFeed(Site, time.Duration(conf.Range)*time.Hour)
+		if err != nil {
+			logger.Error("failed to fetch rss", zap.String("サイト名", Site.Name))
+		}
 		logger.Info(fmt.Sprintf("new feed count: %d", len(f)), zap.String("サイト名", Site.Name))
 		feed = append(feed, f...)
 	}
 	logger.Info(fmt.Sprintf("total feed count: %d", len(feed)))
 }
 
-func SearchFeed(site constant.Site, searchRange time.Duration) []Feed {
-	var feed []Feed
-
+func SearchFeed(site constant.Site, searchRange time.Duration) ([]Feed, error) {
 	fp := gofeed.NewParser()
-	fd, _ := fp.ParseURL(site.URL)
+	fd, err := fp.ParseURL(site.URL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse URL: %w", err)
+	}
 
+	feed := make([]Feed, 0, len(fd.Items))
 	for _, item := range fd.Items {
-		t, _ := time.Parse(site.DateTimeFormat, item.Published)
-		if time.Now().Add(searchRange).Before(t) {
+		publishedTime, err := time.Parse(site.DateTimeFormat, item.Published)
+		if err != nil {
+			continue
+		}
+		if publishedTime.Add(searchRange).After(time.Now()) {
 			feed = append(feed, Feed{
 				Title: item.Title,
 				URL:   item.Link,
@@ -47,5 +55,5 @@ func SearchFeed(site constant.Site, searchRange time.Duration) []Feed {
 		}
 	}
 
-	return feed
+	return feed, nil
 }
